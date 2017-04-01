7e82259c43fe101c1bf739a1ba01677b74a87143
Ryan Petschek <petschekr@gmail.com>
2017-04-01 02:52:13 -0400

    Add custom middleware and define routing structure

--- server.ts
+++ server.ts
@@ -5,6 +5,10 @@ const secp256k1 = require("secp256k1");
 import * as express from "express";
 import * as serveStatic from "serve-static";
 import * as compression from "compression";
+import * as session from "express-session";
+import * as connectMongo from "connect-mongo";
+const MongoStore = connectMongo(session);
+import * as slug from "slug";
 
 export let app = express();
 app.use(compression());
@@ -15,23 +19,59 @@ app.use(compression());
 import * as mongoose from "mongoose";
 (<any>mongoose).Promise = global.Promise;
 mongoose.connect("mongodb://localhost/gatekeeper");
+app.use(session({
+	secret: JSON.parse(fs.readFileSync("keys.json", "utf8")).sessionSecret,
+	cookie: {
+        "path": "/",
+	    "maxAge": 24 * 60 * 60,
+	    "secure": false,
+	    "httpOnly": true
+    },
+	resave: false,
+	store: new MongoStore({
+		mongooseConnection: mongoose.connection,
+		touchAfter: 24 * 60 * 60 // Check for TTL every 24 hours at minimum
+	}),
+	saveUninitialized: true
+}));
 
-interface IBuilding {
+interface IDocument {
     _id: mongoose.Types.ObjectId;
+}
+
+interface IUser extends IDocument {
     name: string;
+    buildingSlug: string;
+    room: string;
+    publicKey: string;
+}
+type IUserMongoose = IUser & mongoose.Document;
+const User = mongoose.model<IUserMongoose>("User", new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true
+    },
+    buildingSlug: {
+        type: String,
+        unique: true
+    },
+    room: String,
+    publicKey: String
+}));
+
+interface IBuilding extends IDocument {
+    name: string;
+    nameSlug: string;
     description: string;
     pictureURL: string;
     location: string;
 
     access: {
         residents: {
-            name: string;
-            room: string;
-            publicKey: string;
+            id: string;
         }[];
         guests: {
-            name: string;
-            publicKey: string;
+            id: string;
             expiration: Date;
         }[];
     };
@@ -41,11 +81,16 @@ interface IBuilding {
     adminAccount: {
         hash: string;
         salt: string;
+        sessionKeys: string[];
     }
 }
 type IBuildingMongoose = IBuilding & mongoose.Document;
 const Building = mongoose.model<IBuildingMongoose>("Building", new mongoose.Schema({
     name: {
+        type: String,
+        unique: true
+    },
+    nameSlug: {
         type: String,
         unique: true
     },
@@ -70,21 +115,10 @@ const Building = mongoose.model<IBuildingMongoose>("Building", new mongoose.Sche
     publicKey: String,
     adminAccount: {
         hash: String,
-        salt: String
+        salt: String,
+        sessionKeys: [String]
     }
 }));
-
-async function getHashForPassword(password: string, salt: Buffer) {
-    return new Promise<Buffer>((resolve, reject) => {
-        crypto.pbkdf2(password, salt, 100000, 256, "sha256", (err, hash) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(hash);
-        });
-    });
-}
 
 async function setDefault() {
 	if (await Building.find({ "name": "Hopkins" }).count() === 0) {
@@ -94,8 +128,10 @@ async function setDefault() {
         let salt = crypto.randomBytes(32);
         let hash = await getHashForPassword(adminPasswordDefault, salt);
 
+        let buildingName = "Hopkins";
 		await new Building({
-            name: "Hopkins",
+            name: buildingName,
+            nameSlug: slug(buildingName).toLowerCase(),
             description: "The best dormitory at Georgia Tech",
             pictureURL: "http://housing.gatech.edu/halls/Building%20Main%20Picture/094Main.jpg",
             location: "115 Bobby Dodd Way, Atlanta, GA 30313",
@@ -109,7 +145,8 @@ async function setDefault() {
             publicKey: publicKey.toString("hex"),
             adminAccount: {
                 hash: hash.toString("hex"),
-                salt: salt.toString("hex")
+                salt: salt.toString("hex"),
+                sessionKeys: []
             }
 		}).save();
         console.info(`Added default building and admin user account with password: ${adminPasswordDefault}`);
@@ -120,6 +157,18 @@ setDefault();
 ///
 /// Common functions
 ///
+async function getHashForPassword(password: string, salt: Buffer) {
+    return new Promise<Buffer>((resolve, reject) => {
+        crypto.pbkdf2(password, salt, 100000, 256, "sha256", (err, hash) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(hash);
+        });
+    });
+}
+
 function readFileAsync(filename: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
         fs.readFile(filename, "utf8", (err, data) => {
@@ -131,11 +180,66 @@ function readFileAsync(filename: string): Promise<string> {
         });
     });
 }
+async function loadBuilding(request: express.Request, response: express.Response, next: express.NextFunction) {
+    let buildingSlug = request.params.building as string;
+    try {
+        let building = await Building.findOne({ "nameSlug": buildingSlug });
+        if (!building) {
+            response.status(400).json({
+                "error": "Invalid building"
+            });    
+            return; 
+        }
+        response.locals.building = building;
+        next();
+    }
+    catch (err) {
+        console.error(err);
+        response.status(500).json({
+            "error": "An unexpected server error occurred"
+        });
+    }
+}
+async function isAdmin(request: express.Request, response: express.Response, next: express.NextFunction) {
+    let sessionKey = request.session!.key;
+    let buildingForAdmin = await Building.findOne({ "adminAccount.sessionKeys": sessionKey });
+    if (!buildingForAdmin || buildingForAdmin.nameSlug !== (response.locals.building as IBuilding).nameSlug) {
+        response.status(401).json({
+            "error": "You are not authorized to access that endpoint"
+        });
+    }
+    next();
+}
 
+///
+/// API
+///
+let apiRouter = express.Router();
 
-app.route("/").get((request, response) => {
-    response.send("GateKeeper");
+apiRouter.route("/addresident").post(isAdmin, async (request, response) => {
+
 });
+
+apiRouter.route("/access").post(async (request, response) => {
+    
+});
+
+///
+/// User facing routes
+///
+let mainRouter = express.Router();
+
+app.route("/").all((request, response) => {
+    // Hopkins is the default building for now
+    response.redirect("/hopkins");
+}); 
+
+mainRouter.route("/").get((request, response) => {
+    response.send("Hopkins");
+});
+
+mainRouter.use("/api", apiRouter);
+app.use("/:building", loadBuilding, mainRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
